e93e450ee66a526c0b2f654e1f2d5e5feb63b48a
Longping Fu <lpfu46@gatech.edu>
2025-02-05 11:19:19 -0500

    add peptide_result_raw.xlsx

--- data_analysis/data_import.R
+++ data_analysis/data_import.R
@@ -2,3 +2,21 @@
 packages_names <- c('tidyverse', 'readxl', 'writexl')
 lapply(packages_names, require, character.only = TRUE)
 
+#load result file
+peptide_result_raw <- read_csv(
+  'data_source/raw_data/ronghuwulab_1738769658_peptide.csv',
+  col_names = TRUE, name_repair = 'universal'
+) |> 
+  filter(str_detect(Reference, 'HUMAN')) |> 
+  filter(!str_detect(Reference, 'contaminant')) |> 
+  filter(!str_detect(Reference, '##')) |> 
+  filter(XCorr > 1.2) |> 
+  filter(PPM > -10, PPM < 10) |> 
+  filter(Sum.Sn > 45) |> 
+  select(Peptide = Trimmed.Peptide, Pept.Length = Pept..Length, Start.Position, End.Position, Trypticity, MissedCleav,
+         Obs.m.z, XCorr, PPM, Reference, Gene.Symbol, Annotation, 
+         cfz_1 = ..126.Sn, cfz_2 = ..127n.Sn, cfz_3 = ..127c.Sn,
+         cfz_4H10_4 = ..128n.Sn, cfz_4H10_5 = ..128c.Sn, cfz_4H10_6 = ..129c.Sn,
+         DMSO_7 = ..129c.Sn, DMSO_8 = ..130n.Sn, DMSO_9 = ..130c.Sn, Sum.Sn)
+
+write_xlsx(peptide_result_raw, path = 'data_source/raw_data/peptide_result_raw.xlsx')
