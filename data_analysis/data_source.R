0363d64dd4e5e0ff60c08c996cea5c5ffb792de6
Longping Fu <lpfu46@gatech.edu>
2025-02-05 13:49:05 -0500

    modify data_source.R

--- data_analysis/data_source.R
+++ data_analysis/data_source.R
@@ -6,4 +6,7 @@ lapply(packages_names, require, character.only = TRUE)
 peptide_result_raw <- read_xlsx('data_source/raw_data/peptide_result_raw.xlsx')
 protein_result_raw <- read_xlsx('data_source/raw_data/protein_result_raw.xlsx')
 
-#
+#quantification
+protein_quantification <- read_xlsx('data_source/quantification/protein_quantification.xlsx')
+
+#normalized data
